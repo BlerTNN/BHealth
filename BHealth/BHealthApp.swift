52ce42c46563a1094f01acaa5fd0741ea0c4f5ef
BlerTNN <tydnzs@icloud.com>
2026-06-26 20:25:08 +0800

    Initial Commit

---
+++ BHealth/BHealthApp.swift
@@ -0,0 +1,32 @@
+//
+//  BHealthApp.swift
+//  BHealth
+//
+//  Created by Bill on 2026-06-26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BHealthApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
